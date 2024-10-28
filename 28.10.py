6c9ad4552f58a20650b640c8ce8dcf15a3d8ae20
VladMorgunovv <morgunovvlad2000@gmail.com>
2024-10-28 20:37:52 +0700

    28

---
+++ 28.10.py
@@ -0,0 +1,93 @@
+s = input().replace("3", "" and )
+print(s)
+# num =""
+# for i in s:
+#     if i !="6":
+#         num += i
+# print(int(num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+# s = input().lower().replace(" ","")
+# print(s)
+# reverse_s = s[::-1]
+# print(reverse_s)
+# if s == reverse_s:
+#     print("является полиндром")
+# else:
+#     print("не является полиндром")
+# s1 = input().split(" ")
+# s2 = input().split(" ")
+# numbers = []
+# s = s1+s2
+# for i in s1:
+#     numbers1.append(int(i))
+# numbers1 = list(set(numbers1))
+# print(numbers1)
+# for i in s2:
+#     numbers2.append(int(i))
+# numbers2 = list(set(numbers2))
+# print((numbers2))
+# numbers1 =max([int(s) for s in s1]),min([int(s) for s in s1])
+# numbers2 =max([int(s) for s in s2]),min([int(s) for s in s2])
+# print(numbers1)
+# print(numbers2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# result = []
+# for i in range(100,1000):
+#     s = str(i)
+#     num = []
+#     for j in s:
+#         num.append(j)
+#     if num[0] == num[1] or num[1] == num[2] or num[0] == num[2]:
+#             if num[0] == num[1] and == num[2]:
+#                 print()
+#             else:
+#                 result.append(int(num[0]+num[1]+num[2]))
+# print(len(result))
+
+# s = input().split(" ")
+# numbers = []
+# for i in range(int(s[0]), int(s[1]) +1):
+#     num = []
+#     for x in str(i):
+#         num.append(x)
+#     for j in range(10):
+#         if num.count(str(j)) == 2:
+#             numbers.append(i)
+# print(len(numbers))
+#
+# for i in range(30):
+#     print(" " * i, "*" * (30-i * 2),)
