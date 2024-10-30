0610d726de7066271eecc293a031d93ea611296c
VladMorgunovv <morgunovvlad2000@gmail.com>
2024-10-30 20:37:52 +0700

    30

---
+++ 30.10.py
@@ -0,0 +1,40 @@
+# # import  as pd
+# # # list23 = [134,152,522,31234]
+# #
+# # #
+# # # d = {0: "wewe", "car": "232323", 4:"soso"}
+# # # # print(d["car"])
+# # # # d.pop("car2")
+# # # # print(d)
+# # # print(d.keys())
+# # # print(d.values())
+# # # for keys, values in d.items():
+# # #     if keys == "car":
+# # #         print(keys,":", values)
+# # c =(1,2,3,4,5,54,8,8)
+# # print(min(c))
+# # l = ['a','b','c','d','e','f','g','h']
+# # d1={}
+# # for c,l in zip(c, l):
+# #
+# #     d1[c] = l
+# # print(d1)
+# # d1 = .DataFrame()
+# # pri
+# import  re
+# s =input("видите логин")
+# pattern = '(User)+'
+# st = re.findall(pattern, s)
+# print(st)
+# if not st:
+#
+
+
+
+string = input().split(" ")
+max_squre =0
+for i in range(int(string[0]), int(string[1])):
+    sqrt = i ** 0.5
+    if i ==
+
+    p
--- 9.py
+++ 9.py
@@ -1,11 +1,7 @@
-string = int(input().split( ")
-# if int(string[0]) == int(string[1]):
-#     print("числа ровны ",string[0])
-# else:
-#     if int(string[0]) > int(string[1]):
-#         print("2 чесло:", int(string[1]))
-#         print("1 чесло:", int(string[0]))
-#     else:
-#         print("1 чесло:", int(string[0]))
-#       print("2 чесло:", int(string[1]))
+string = input().split(" ")
+max_squre = 0
+for i in range(int(string[0]), int(string[1])):
+    sqrt = i * i
+    if i ==
 
+    p
