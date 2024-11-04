22794c7493ddffb8217a2ae818690e387e41681b
VladMorgunovv <morgunovvlad2000@gmail.com>
2024-11-04 20:24:57 +0700

    04

---
+++ 4.2.py
@@ -0,0 +1,16 @@
+# spisok = [-1, -2, -4, -5, 0, 3, 3,5, 6, 63, 3, 4]
+# positiv = 0
+# negativ = 0
+# nills = 0
+# print("максимальное число:",max(spisok))
+# print("минимальное число:",min(spisok))
+# for x in spisok:
+#     if x <0:
+#         negativ +=1
+#     if x >0:
+#         positiv +=1
+#     if x == 0:
+#         nills +=1
+# print("положительные числа:",positiv, "отрецательное числа:",negativ, nills, end="")
+#
+#
